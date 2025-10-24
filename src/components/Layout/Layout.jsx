56ae02ccf3fc84b5ba2a87abd843a3b90321e63f
mohamedmoustafaa <mohamedmooostaf@gmail.com>
2025-10-24 06:18:31 +0300

    Fix Navbar and Footer import in Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,8 @@
- import styles from './layout.module.css';
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import { Outlet } from 'react-router-dom';
+import styles from './layout.module.css';
 
 export default function Layout() {
   return (
